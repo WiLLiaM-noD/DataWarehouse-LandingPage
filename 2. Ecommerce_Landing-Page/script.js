382612165242d2f1f3967c7cac44fceb9369f051
WiLLiaM-noD <williamkurniawan1144@gmail.com>
2021-02-18 10:17:30 +0700

    Nav Link Updated

--- 2. Ecommerce_Landing-Page/script.js
+++ 2. Ecommerce_Landing-Page/script.js
@@ -1,3 +1,21 @@
+// Nav Link
+const navs = document.querySelectorAll('.nav__links');
+console.log(navs);
+
+navs.forEach(nav => {
+  nav.addEventListener('click', e => {
+    e.preventDefault();
+    let childLink = nav.childNodes[1].getAttribute('href');
+    console.log(childLink);
+    document.querySelector(childLink).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+
+// Date
 const dateLabel = document.querySelector('#date');
+
+// Getting Year
 const currentYear = new Date().getFullYear();
 dateLabel.textContent = currentYear;
