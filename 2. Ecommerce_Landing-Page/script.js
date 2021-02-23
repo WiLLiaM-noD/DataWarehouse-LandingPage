9c1fea378292276c31b2ca76f9adf4a186e0e027
WiLLiaM-noD <williamkurniawan1144@gmail.com>
2021-02-23 09:13:51 +0700

    Add Reveale Animation

--- 2. Ecommerce_Landing-Page/script.js
+++ 2. Ecommerce_Landing-Page/script.js
@@ -18,3 +18,24 @@ const dateLabel = document.querySelector('#date');
 // Getting Year
 const currentYear = new Date().getFullYear();
 dateLabel.textContent = currentYear;
+
+// Animation
+const allSub = document.querySelectorAll('section');
+
+const showSection = (entries, observer) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const subObserver = new IntersectionObserver(showSection, {
+  root: null,
+  threshold: 0.35,
+});
+
+allSub.forEach(sub => {
+  subObserver.observe(sub);
+  sub.classList.add('section--hidden');
+});
